7df2f4c54b6b1a933f80961d452ef6f19c9b2bbb
iamsudhakar <sudhakarm761998@gmail.com>
2020-11-24 10:30:10 +0530

    Application 1.1

--- JDBC_1/src/org/jsp/demo_1/Demo2.java
+++ JDBC_1/src/org/jsp/demo_1/Demo2.java
@@ -3,6 +3,7 @@ package org.jsp.demo_1;
 public class Demo2 {
 	public void main()
 	{
-	System.out.println("Demo tesing application");
+	System.out.println("Demo tesing application Started");
+	System.out.println("Demo tesing application Ended");
 	}
 }
